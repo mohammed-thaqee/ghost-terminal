192113a338bac539dee0236b62680ca8242d7ec4
mohammed-thaqee <mohammedthaqeep@gmail.com>
2026-03-23 10:33:21 +0530

    Update oneko.sh

--- modules/oneko.sh
+++ modules/oneko.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-echo "[*] oneko module starting..."
-
-# Install oneko
 sudo apt-get update -y >/dev/null 2>&1
 sudo apt-get install -y oneko >/dev/null 2>&1
 
-# Run oneko
 oneko -tora &
-
-echo "[✔] oneko running"
-
-# Keep terminal alive briefly
-sleep 10
